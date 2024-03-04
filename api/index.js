1686aad2ae8e82b76547f8896de98f537d4cafe1
Deepak Kumar <deepaklg02@gmail.com>
2024-03-05 00:41:01 +0530

    Adding  CORS

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-// const cors = require("cors");
+const cors = require("cors");
 const connectToDb = require("../db");
 const User = require("../model");
 const bcrypt = require("bcryptjs");
@@ -8,7 +8,7 @@ const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
 connectToDb();
 
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
